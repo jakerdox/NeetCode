cb066af20bb8d5e1b5b3900e49a16b005548f734
Jake Doxey <jakedoxey@gmail.com>
2024-06-17 10:27:41 -0700

    Updating the repository with 0020. Valid Parentheses

---
+++ src/0020_valid_parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = {")": "(", "}": "{", "]": "["}
+
+        for c in s:
+            if c not in closeToOpen:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != closeToOpen[c]:
+                return False
+            stack.pop()
+
+        return not stack
